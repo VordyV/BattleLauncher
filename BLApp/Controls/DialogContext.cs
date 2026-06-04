af31509782add79eee68f64ae4a7405cc4c16fc3
vordy <vorklab@outlook.com>
2026-06-04 19:51:47 +0300

    add DialogContext.cs

---
+++ BLApp/Controls/DialogContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Irihi.Avalonia.Shared.Contracts;
+
+namespace BLApp.Controls;
+
+public partial class DialogContext : ObservableObject, IDialogContext  
+{  
+    public void Close()  
+    {  
+        RequestClose?.Invoke(this, true);  
+    }  
+  
+    public event EventHandler<object?>? RequestClose;  
+}
