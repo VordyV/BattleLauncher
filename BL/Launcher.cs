a14bac39acbacac88abca0b4cd9a017ae394e54b
vordy <vorklab@outlook.com>
2026-06-04 19:53:02 +0300

    add Configurator, method AddGameRegistry

--- BL/Launcher.cs
+++ BL/Launcher.cs
@@ -2,18 +2,20 @@
 
 public class Launcher
 {
+    public Configurator Configurator;
+    
     protected Dictionary<string, Game> Games;
     
     public Launcher(string pathConfig)
     {
-        
         this.Games = new();
+        this.Configurator = new Configurator(pathConfig);
     }
 
-    public void RegisterGame(string gameId, string name, string shortName)
+    public void RegisterGame(string gameId, string name, string shortName, string[] determinants)
     {
         if (this.Games.ContainsKey(gameId)) throw new Exception($"A game with this ID '{gameId}' is already registered");
-        this.Games.Add(gameId, new Game(launcher: this, id: gameId, name: name, shortName: shortName));
+        this.Games.Add(gameId, new Game(launcher: this, id: gameId, name: name, shortName: shortName, determinants: determinants));
     }
 
     public Game GetGame(string gameId)
@@ -23,4 +25,12 @@ public class Launcher
     }
 
     public List<string> GetGames() => this.Games.Keys.ToList();
+
+    public void AddGameRegistry(string gameId, string path)
+    {
+        Game game = this.GetGame(gameId);
+        if (game.IsInstall) throw new Exception($"Game '{gameId}' is already in the registry");
+        this.Configurator.AddSection(gameId);
+        this.Configurator.Set(gameId, "path", path);
+    }
 }
