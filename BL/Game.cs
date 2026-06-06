5393ba62485b4f1f2d2e206b9fd636574f9d4298
vordy <vorklab@outlook.com>
2026-06-06 22:47:53 +0300

    add fields ReferenceClient, Client; add methods CheckPathInstall, GenerateDefaultClient, GetPath, SetReferenceClient, GetReferenceClient, SetCurrentClient

--- BL/Game.cs
+++ BL/Game.cs
@@ -8,6 +8,8 @@ public class Game
     public GameStatus Status { get; protected set; } = GameStatus.NotInstalled;
     public bool IsInstall { get; protected set; }
     public string[] Determinants { get; }
+    public string ReferenceClient { get; protected set; }
+    public string Client { get; protected set; }
 
     public event Func<GameStatus, Task> OnChangeStatus; 
 
@@ -24,12 +26,19 @@ public class Game
 
     public async Task<bool> CheckInstall()
     {
-        await this.Launcher.Configurator.WaitReadiness();
-        bool isInstall = this.Launcher.Configurator.HasSection(this.Id);
-        this.IsInstall = isInstall;
-        if (isInstall) await this.SetStatus(GameStatus.NotRunning);
+        await this.Launcher.Registry.WaitReadiness();
+        bool isInstall = this.Launcher.Registry.HasSection(this.Id);
+        string? path = this.GetPath();
+        this.IsInstall = isInstall && path != null;
+        if (this.IsInstall) await this.SetStatus(GameStatus.NotRunning);
         else await this.SetStatus(GameStatus.NotInstalled);
         return isInstall;
+    }
+
+    public bool CheckPathInstall()
+    {
+        string? path = this.GetPath();
+        return path != null && Directory.Exists(path);
     }
 
     public void AddGameRegistry(string path)
@@ -55,5 +64,48 @@ public class Game
         }
 
         return true;
+    }
+
+    public async Task GenerateDefaultClient()
+    {
+        await this.Launcher.GenerateDefaultClient(this.Id);
+    }
+
+    public string? GetPath()
+    {
+        return this.Launcher.GetPath(this.Id);
+    }
+
+    public void SetReferenceClient(string? clientId)
+    {
+        this.ReferenceClient = clientId;
+        this.Launcher.SetReferenceClient(this.Id, clientId);
+    }
+    
+    public string? GetReferenceClient()
+    {
+        string? clientId = this.Launcher.GetReferenceClient(this.Id);
+        if (clientId == null) throw new Exception("Reference client has not been set");
+        this.ReferenceClient = clientId;
+        return clientId;
+    }
+    
+    public void SetCurrentClient(string? clientId)
+    {
+        this.Client = clientId;
+        this.Launcher.SetCurrentClient(this.Id, clientId);
+    }
+    
+    public string? GetCurrentClient()
+    {
+        string? clientId = this.Launcher.GetCurrentClient(this.Id);
+        if (clientId == null) throw new Exception("Current client has not been set");
+        this.Client = clientId;
+        return clientId;
+    }
+
+    public async Task<ClientData[]> GetClients()
+    {
+        return await this.Launcher.GetClients(this.Id);
     }
 }
