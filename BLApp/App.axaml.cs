11606d92d928ca384014418493770026527b5e4f
vordy <vorklab@outlook.com>
2026-06-15 22:13:09 +0300

    add launch params bf2142

--- BLApp/App.axaml.cs
+++ BLApp/App.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
@@ -13,8 +14,18 @@ public partial class App : Application
     public override void Initialize()
     {
         this.Launcher = new Launcher(pathConfig: "bnconfig.registry");
-        
-        this.Launcher.RegisterGame(gameId: "bf2142", name: "Battlefield 2142", shortName: "BF2142", determinants: new []{"bf2142.exe"});
+
+        ILaunchParam[] launchParams = new ILaunchParam[]
+        {
+            new LaunchParamBool() {Id = "fullscreen", Name = "Full screen"},
+            new LaunchParamDict() {Id = "resolution", Name = "Resolution", Dictionary = new()
+            {
+                {"1920x1080", "+szx 1920 +szy 1080"},
+                {"1280x720", "+szx 1280 +szy 720"}
+            }}
+            //new LaunchParamDict() {Id = "resolution", Name = "Resolution"}
+        };
+        this.Launcher.RegisterGame(gameId: "bf2142", name: "Battlefield 2142", shortName: "BF2142", determinants: new []{"bf2142.exe"}, launchParams: launchParams);
         
         AvaloniaXamlLoader.Load(this);
     }
