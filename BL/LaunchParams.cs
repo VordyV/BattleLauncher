c17c29757300ad19b7f3fec9b29becbf75f0143f
vordy <vorklab@outlook.com>
2026-06-15 22:14:16 +0300

    new class

---
+++ BL/LaunchParams.cs
@@ -0,0 +1,41 @@
+﻿namespace BL;
+
+public abstract class ILaunchParam
+{
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public bool FullFormat { get; set; } = false;
+};
+
+public abstract class LaunchParam <T> : ILaunchParam
+{
+    public abstract IEnumerable<string> BuildArgs(T param);
+}
+
+public class LaunchParamStr : LaunchParam<string>
+{
+    public override IEnumerable<string> BuildArgs(string param)
+    {
+        yield return $"+{this.Id} {param}";
+    }
+}
+
+public class LaunchParamBool : LaunchParam<bool>
+{
+    public override IEnumerable<string> BuildArgs(bool param)
+    {
+        string value = param ? "1" : "0";
+        yield return $"+{this.Id} {value}";
+    }
+}
+
+public class LaunchParamDict : LaunchParam<string>
+{
+    public Dictionary<string, string> Dictionary { get; set; } = new();
+    
+    public override IEnumerable<string> BuildArgs(string param)
+    {
+        if (this.Dictionary.TryGetValue(param, out var args))
+            yield return args;
+    }
+}
